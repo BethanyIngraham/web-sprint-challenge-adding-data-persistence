5a31cffa35d5a99a7a49d17715e683ecad5be4f8
Bethany Ingraham <BethFIngraham@gmail.com>
2024-10-06 14:21:35 -0400

    index, server and routers wired - work in progress

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,15 @@
-// build your `/api/projects` router here
+const express = require('express');
+
+const router = express.Router();
+
+router.use('*', (req, res) => {
+    res.json('testing project router')
+});
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message || 'Something went wrong with the server'
+    })
+});
+
+module.exports = router;
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,15 @@
-// build your `/api/tasks` router here
+const express = require('express');
+
+const router = express.Router();
+
+router.use('*', (req, res) => {
+    res.json('testing task router')
+});
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message || 'Something went wrong with the server'
+    })
+});
+
+module.exports = router;
