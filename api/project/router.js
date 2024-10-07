dd61f138c9440317d508a7e8016349cc36ce4371
Bethany Ingraham <BethFIngraham@gmail.com>
2024-10-06 20:00:44 -0400

    database migrations complete, work in progress

--- api/project/router.js
+++ api/project/router.js
@@ -22,7 +22,8 @@ router.post('/', (req, res, next) => {
 
 router.use((err, req, res, next) => { // eslint-disable-line
     res.status(err.status || 500).json({
-        message: err.message || 'Something went wrong with the server'
+        message: err.message || 'Something went wrong with the server',
+        stack: err.stack
     })
 });
 
