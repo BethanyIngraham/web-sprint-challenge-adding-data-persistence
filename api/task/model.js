497aef0bedf58f1b82cbbdccebf19651308c4d95
Bethany Ingraham <BethFIngraham@gmail.com>
2024-10-11 20:24:23 -0400

    Tasks GET endpoint and model function working

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,28 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+async function getTasks() {
+    const tasks = await db('tasks as t')
+        .join('projects as p', 't.project_id', 'p.project_id')
+        .select(
+            'task_id', 'task_description',
+            'task_notes', 'task_completed',
+            'project_name', 'project_description'
+        )
+
+    tasks.forEach(row => {
+        row.task_completed = 
+        row.task_completed === 1 ? 
+         true : false
+    })
+
+    return tasks
+}
+
+function createTask() {
+    
+}
+
+module.exports = {
+    getTasks,
+    createTask
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const Tasks = require('./model');
 
 const router = express.Router();
 
-router.use('*', (req, res) => {
-    res.json('testing task router')
+router.get('/', async (req, res, next) => {
+    try {
+        const tasks = await Tasks.getTasks();
+        res.json(tasks);
+    } catch(err) {
+        next(err);
+    }
+});
+
+router.post('/', (req, res, next) => {
+
 });
 
 router.use((err, req, res, next) => { // eslint-disable-line
